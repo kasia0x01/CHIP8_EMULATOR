765cfc455c5a655729ae6af91d3836e6d9656560
Kate <embeddedkate@gmail.com>
2026-02-13 15:58:45 +0100

    Try new project structure

---
+++ CMakeFiles/CHIP8.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CHIP8"
+  "CHIP8.pdb"
+  "CMakeFiles/CHIP8.dir/application.cpp.o"
+  "CMakeFiles/CHIP8.dir/application.cpp.o.d"
+  "CMakeFiles/CHIP8.dir/chip8.cpp.o"
+  "CMakeFiles/CHIP8.dir/chip8.cpp.o.d"
+  "CMakeFiles/CHIP8.dir/chip8_impl.cpp.o"
+  "CMakeFiles/CHIP8.dir/chip8_impl.cpp.o.d"
+  "CMakeFiles/CHIP8.dir/main.cpp.o"
+  "CMakeFiles/CHIP8.dir/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CHIP8.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
